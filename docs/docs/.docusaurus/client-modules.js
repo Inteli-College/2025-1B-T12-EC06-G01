48641f608f1e0de0d44adf678a67dae30aa42bfc
Murilo Couto Oliveira <159152280+mcouto0xml@users.noreply.github.com>
2025-04-22 17:47:30 -0300

    Delete docs/docs directory

--- docs/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/inteli/Documentos/2025-1B-T12-EC06-G01/docs/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/inteli/Documentos/2025-1B-T12-EC06-G01/docs/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/inteli/Documentos/2025-1B-T12-EC06-G01/docs/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/inteli/Documentos/2025-1B-T12-EC06-G01/docs/docs/src/css/custom.css"),
-];
