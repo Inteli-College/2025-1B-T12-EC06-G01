c1627698bc01c24d74333d482a570c784cd46760
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-05-09 11:23:17 -0300

    feat: inicializa servidor react

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rooster</h1>
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
