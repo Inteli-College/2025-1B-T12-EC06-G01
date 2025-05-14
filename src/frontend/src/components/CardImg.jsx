d9ff3f0095970e220eafbc3265ad291447dadfd8
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-05-14 02:01:39 -0300

    feat: implementa checkbox customizado no componente CardImg

--- src/frontend/src/components/CardImg.jsx
+++ src/frontend/src/components/CardImg.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const Card = styled.div`
     width: 90%;
@@ -24,7 +25,39 @@ const Card = styled.div`
       border: none;
       padding: 62px;
     }
-      
+
+`
+
+const CheckboxWrapper = styled.label`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    gap: 8px;
+    margin-bottom: .5rem;
+    z-index: 1;
+
+    input[type="checkbox"] {
+      display: none;
+    }
+    .custom-checkbox {
+      width: 20px;
+      height: 20px;
+      border: 2px solid #555;
+      border-radius: 4px;
+      background-color: white;
+      position: relative;
+      background-color:rgb(112, 112, 112);
+    }
+
+    input[type="checkbox"]:checked + .custom-checkbox::after {
+      content: "✔";
+      position: absolute;
+      left: 3px;
+      top: -1px;
+      font-size: 18px;
+      color: white;
+      background-color: #545454;
+    }
 `
 
 const ImagemCard = styled.div`
@@ -34,14 +67,24 @@ const ImagemCard = styled.div`
 `
 
 export default function CardImg() {
+  const [checked, setChecked] = useState(false);
+
   return (
     <Card>
-        <div className='topo-card-img'>
-          <p width="80%">Título da imagem</p>
-          <input type='checkbox' />
-        </div>
+      <div className='topo-card-img'>
+        <p width="80%">Título da imagem</p>
+        <CheckboxWrapper>
+          <input
+            type="checkbox"
+            id="myCheckbox"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+          />
+          <span className="custom-checkbox" />
+        </CheckboxWrapper>
+      </div>
 
-        <div style={{width: '100%', height: '87%', backgroundColor: '#fff'}} />
+      <div style={{ width: '100%', height: '87%', backgroundColor: '#fff' }} />
     </Card>
   )
 }
