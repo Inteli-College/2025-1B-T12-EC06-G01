809d42a89ae0829c792cbfd35fed547f9090c19c
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-05-15 22:03:29 -0300

    feat: atualiza a estilização da sidebar

--- src/frontend/src/components/Sidebar.jsx
+++ src/frontend/src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { IoMdDocument } from "react-icons/io"
-import { FaRegUserCircle, FaUpload } from "react-icons/fa"
+import { IoIosAdd } from "react-icons/io"
+import { FaRegUserCircle, FaUpload, FaFolder } from "react-icons/fa"
+import logo from '../logo.svg'
 
 const Container = styled.div`
   width: 18vw;
@@ -24,16 +25,28 @@ const BntMaior = styled.button`
   width: 80%;
   height: 150px;
   border-radius: 20px;
-  background-color: #8F8F8F;
+  background-color: #629EBC;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: none;
-  
+  border: 5px solid #2e2e2e;
+  padding: 1rem;
   transition: background-color 0.3s ease; 
+
   &:hover {
-    background-color: #6f6f6f; 
+    background-color: #3D80A3; 
     cursor: pointer; 
+  }
+
+  span, svg {
+    font-size: 32px;
+    color: #fff;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  svg {
+    font-size: 72px;
   }
 `
 
@@ -44,7 +57,7 @@ const Recente = styled.div`
 
   svg {
     font-size: 1.5rem;
-    color: #8F8F8F;
+    color: #969FB0;
   }
 
   a {
@@ -63,16 +76,19 @@ const BtnMenor = styled.button`
   width: 80%;
   height: 123px;
   border-radius: 20px;
-  background-color: #8F8F8F;
+  background-color: #629EBC;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: none;
+  border: 5px solid #145E7A;
+  color: #fff;
+  font-size: 28px;
+  text-transform: uppercase;
   
   transition: background-color 0.3s ease; 
 
   &:hover {
-    background-color: #6f6f6f; 
+    background-color: #3D80A3; 
     cursor: pointer; 
   }
 `
@@ -84,8 +100,10 @@ const Perfil = styled.div`
   justify-content: space-around;
   gap: .3rem;
   padding: .5rem;
+  color: #fff;
 
-  background-color: #8F8F8F;
+  background-color: #326886;
+  border: 5px solid #145E7A;
   border-radius: 20px;
 
 
@@ -96,15 +114,19 @@ const Perfil = styled.div`
   button {
     width: 20%; 
     height: 70%; 
-    background-color: #545454;
+    background-color: #629EBC;
     border: none;
     border-radius: 10px;
     transition: background-color 0.3s ease; 
   }
 
   button:hover {
-    background-color:rgb(60, 60, 60); 
+    background-color: #3D80A3; 
     cursor: pointer; 
+  }
+
+  p {
+    text-transform: uppercase;
   }
 `
 
@@ -174,9 +196,12 @@ export default function Sidebar() {
 
   return (
     <Container>
-      <div style={{ width: '80%', height: 100, backgroundColor: '#fff' }} />
+      <img src={logo} width='45%' alt='ovo com rachadura' />
 
-      <BntMaior onClick={togglePopup}>Adicionar Projeto</BntMaior>
+      <BntMaior onClick={togglePopup}>
+        <span>Novo Projeto</span>
+        <IoIosAdd />
+      </BntMaior>
       {showPopup && (
         <Popup>
           <div>
@@ -194,39 +219,43 @@ export default function Sidebar() {
             </div>
             <button onClick={togglePopup}>concluir</button>
           </div>
-        </Popup>        
+        </Popup>
       )}
 
       <section>
         <Recente>
-          <IoMdDocument />
+          <FaFolder />
           <a href='/'>Lorem ipsum dolor sit amet</a>
         </Recente>
 
         <Recente>
-          <IoMdDocument />
+          <FaFolder />
           <a href='/'>Lorem ipsum dolor sit amet</a>
         </Recente>
 
         <Recente>
-          <IoMdDocument />
+          <FaFolder />
           <a href='/'>Lorem ipsum dolor sit amet</a>
         </Recente>
       </section>
 
-      <hr style={{width: '80%'}} />
+      <hr style={{ width: '80%' }} />
 
-      <section style={{width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1.8rem'}}>
-        <BtnMenor>Botão</BtnMenor>
-        <BtnMenor>Botão</BtnMenor>
+      <section style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1.8rem' }}>
+        <BtnMenor>Projetos</BtnMenor>
+        <BtnMenor>Dashboard</BtnMenor>
       </section>
 
       <Perfil>
         <FaRegUserCircle />
-        <p>Nome do usuáro</p>
+        <p>
+          <strong>Nome do usuáro</strong>
+          <br />
+          ID:12345
+        </p>
         <button>bnt</button>
       </Perfil>
-      
+
     </Container>
   )
 }
