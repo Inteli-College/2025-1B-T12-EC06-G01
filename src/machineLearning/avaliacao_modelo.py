01780f7040e683ce424ec6f56f20342fbada3fd7
Pietra Batista <pietra.batista@sou.inteli.edu.br>
2025-05-18 23:35:03 -0300

    feat: novo teste e aprimoramento

--- src/machineLearning/avaliacao_modelo.py
+++ src/machineLearning/avaliacao_modelo.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
 
-# Etapa 4: Avaliação
-model = YOLO("C:/Users/pietr/Documents/modulo6/2025-1B-T12-EC06-G01/runs/classify/train5/weights/best.pt")
+model = YOLO("/runs/classify/train11/weights/best.pt")
 metrics = model.val()
 
 print(f"Acurácia top-1: {metrics.top1:.3f}")
--- src/machineLearning/teste.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Carrega o modelo YOLOv8 
-model = YOLO("yolov8n.pt")
-
-# Roda a predição na imagem teste (spoiler: funcionou, mas não detectou nada pq o modelo não reconhce fissuras ainda. por isso, precisamos treinar ele pra isso.)
-results = model.predict(source="people.jpg", show=True)
