9bf82e81191b4e27becd13272f35721a4dbe9671
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-05-22 16:34:42 -0300

    feat: adiciona página de resultado do modelo

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Predio from './pages/Predio';
 import Fachada from './pages/Fachada'; 
 import { ProjectProvider } from './contexts/ProjectContext';
+import Result from './pages/Result';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/predio/:predioNome" element={<Predio />} />
-          <Route path="/predio/:predioNome/:fachadaNome" element={<Fachada />} /> {/* opcional */}
+          <Route path="/predio/:predioNome/:fachadaNome" element={<Fachada />} />
+          <Route path="/resultado" element={<Result />} />
         </Routes>
       </Router>
     </ProjectProvider>
